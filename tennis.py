c19f5fa18bc597e57f6d2a7470d0bfa3450244ce
manighahrmani <mashapicasso@gmail.com>
2023-07-08 18:47:41 +0100

    Add the other examples

---
+++ tennis.py
@@ -0,0 +1,42 @@
+def main():
+    prWinPt, numGames = getInputs()
+    wins = simulateNGames(prWinPt, numGames)
+    printSummary(wins, numGames)
+
+
+def getInputs():
+    prWinPt = float(
+        input("Probability of winning a point (a float between 0 and 1): "))
+    numGames = int(input("Number of games to simulate (a positive integer): "))
+    return prWinPt, numGames
+
+
+def simulateNGames(prWinPt, numGames):
+    wins = 0
+    for game in range(numGames):
+        pointsPl, pointsOp = simulateGame(prWinPt)
+        if pointsPl > pointsOp:
+            wins = wins + 1
+    return wins
+
+
+def simulateGame(prWinPt):
+    from random import random
+    pointsPl, pointsOp = 0, 0
+    while not gameOver(pointsPl, pointsOp):
+        if random() < prWinPt:
+            pointsPl = pointsPl + 1
+        else:
+            pointsOp = pointsOp + 1
+    return pointsPl, pointsOp
+
+
+def gameOver(pointsPl, pointsOp):
+    return (pointsPl >= 4 or pointsOp >= 4) and  \
+        abs(pointsPl - pointsOp) >= 2
+
+
+def printSummary(wins, numGames):
+    proportion = wins / numGames
+    print("Wins:", wins, end="  ")
+    print("Proportion: {0:0.2f}".format(proportion))
