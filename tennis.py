ef3700c007397907ba0f45970539629a6c4efc74
manighahrmani <mashapicasso@gmail.com>
2024-11-25 21:31:43 +0000

    Update tennis.py

--- tennis.py
+++ tennis.py
@@ -1,43 +1,43 @@
+# Lecture 8 - an example of top-down design
 from random import random
 
 
-def main():
-    pr_win_pt, num_games = get_inputs()
-    wins = simulate_n_games(pr_win_pt, num_games)
-    print_summary(wins, num_games)
-
-
 def get_inputs():
-    pr_win_pt = float(
-        input("Probability of winning a point (a float between 0 and 1): "))
-    num_games = int(
-        input("Number of games to simulate (a positive integer): "))
-    return pr_win_pt, num_games
+    point_win_probability = float(input("Probability of winning each point: "))
+    number_of_games = int(input("Games to simulate: "))
+    return point_win_probability, number_of_games
 
 
-def simulate_n_games(pr_win_pt, num_games):
+def simulate_games(point_win_probability, number_of_games):
     wins = 0
-    for game in range(num_games):
-        points_pl, points_op = simulate_game(pr_win_pt)
-        if points_pl > points_op:
-            wins += 1
+    for game in range(number_of_games):
+        points_player, points_opponent = simulate_game(point_win_probability)
+        if points_player > points_opponent:
+            wins = wins + 1
     return wins
 
 
-def simulate_game(pr_win_pt):
-    points_pl, points_op = 0, 0
-    while not game_over(points_pl, points_op):
-        if random() < pr_win_pt:
-            points_pl += 1
+def simulate_game(point_win_probability):
+    points_player, points_opponent = 0, 0
+    while not game_over(points_player, points_opponent):
+        if random() < point_win_probability:
+            points_player = points_player + 1
         else:
-            points_op += 1
-    return points_pl, points_op
+            points_opponent = points_opponent + 1
+    return points_player, points_opponent
 
 
-def game_over(points_pl, points_op):
-    return (points_pl >= 4 or points_op >= 4) and abs(points_pl - points_op) >= 2
+def game_over(points_player, points_opponent):
+    return (points_player >= 4 or points_opponent >= 4) and \
+        abs(points_player - points_opponent) >= 2
 
 
-def print_summary(wins, num_games):
-    print(f"Wins: {wins}")
-    print(f"Total games: {num_games}")
+def print_summary(wins, number_of_games):
+    proportion = wins / number_of_games
+    print(f"Wins: {wins}, proportion: {proportion:.2f}")
+
+
+def main():
+    point_win_probability, number_of_games = get_inputs()
+    wins = simulate_games(point_win_probability, number_of_games)
+    print_summary(wins, number_of_games)
